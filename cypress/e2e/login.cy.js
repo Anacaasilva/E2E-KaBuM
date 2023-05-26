ce8ded0221e68b92db85e1eb1cb7c3c4a15a0dfd
Ana Carolina <anacarolina.oliveira1020@gmail.com>
2023-05-26 13:44:02 -0300

    Configured test environment

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,12 @@
+describe('Auth', () => {
+
+    beforeEach(() => {
+      cy.visit('https://www.kabum.com.br/')
+    });
+
+    describe('My First Test', () => {
+      it('Does not do much!', () => {
+        expect(true).to.equal(true)
+      })
+    })
+});     
